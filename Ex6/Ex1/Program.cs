5a9ec59b199e4a444ae1938b316c0e7b5c73d5a0
Nemésio Florence Vieira Filho <54808745+NemesioFVF@users.noreply.github.com>
2021-02-02 03:24:11 -0300

    Add files via upload

---
+++ Ex6/Ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número: ");
+            Int32 n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("O número é par.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("O número é impar.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
